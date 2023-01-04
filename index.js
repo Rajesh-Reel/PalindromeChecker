b68eea21697b9c3b275c0e0ecb98dd704382bac7
Rajesh-Reel <rajreel2@hotmail.com>
2023-01-04 21:25:49 +0000

    added 2nd variable for comparison purposes

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 function palindrome(myStr) {
+    // below removes special characters, spaces and makes myStr lowercase
+    let removeCharacters = myStr.replace(/[^A-Z0-9]/ig, "").toLowerCase();
+    
+    // reverses above variable for comparison
+    let isPalindrome = removeCharacters.split("").reverse().join("");
+
     // if () { //add condition
     //     // add logic
     // } else {
